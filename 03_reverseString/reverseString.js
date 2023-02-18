f2932511c4dd9ad90b9add1ac44f5422e67cafbd
markosinjo <stojanovic.marko0@outlook.com>
2023-02-18 04:41:07 +0100

    Solve first three problems

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
 
+    const arr = inputString.split("");
+    let reverse = "";
+    console.log(arr);
+
+    while(arr.length !== 0) {
+        reverse = reverse.concat(arr.pop());
+    }
+    return reverse;
 };
-
+reverseString("omega");
 // Do not edit below this line
 module.exports = reverseString;
